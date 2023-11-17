732d89717c3fc78998d08c2df8518aed1baa1b98
shsalman114 <shaik.salman@accellor.com>
2023-11-17 19:26:08 +0530

    updated

--- DMS/assets/script/script.js
+++ DMS/assets/script/script.js
@@ -1,35 +1,33 @@
-// $(':radio').on('click', (event) => {
-//   let radioId = event.currentTarget.id;
-//   if (radioId === 'custom') {
-//     $('#spec').show();
-//     $(':button').on('click', () => {
-//       $('.tab-link').toggleClass('active');
-//     });
-//   } else {
-//     $('#spec').hide();
-//     if ($('#details').hasClass('active')) $('#details').toggleClass('active');
-//     else $('#details').toggleClass('active');
-//   }
-// });
+$(document).ready(function () {
+  $(':radio').on('click', (event) => {
+    if (event.currentTarget.id === 'custom') {
+      $('#spec').show();
+    }
+    if (event.currentTarget.id === 'auto') {
+      console.log('radio else');
+      $('#spec').hide();
+      $('#spec').removeClass('c-active');
+      if (!$('#details').hasClass('c-active')) {
+        $('#details').addClass('c-active');
+      }
+    }
+  });
 
-$(':radio').on('click', (event) => {
-  console.log('radio event')
-  console.log('radio event')
-  console.log('radio event')
-  if (event.currentTarget.id === 'custom') {
-    console.log('inside if')
-    $('#spec').show();
-    $(':button').on('click', () => {
-      $('.tab-link').toggleClass('active');
-    });
-    // if ($('#details').hasClass('active')) $('#details').toggleClass('active');
-    // else $('#details').toggleClass('active');
-  }
-  if (event.currentTarget.id === 'auto') {
-    console.log('radio else')
-    $('#spec').hide();
-  }
+  $('.specification-section').hide();
+
+  $('.tab-link').on('click', (e) => {
+    if ($('#custom')[0].checked) {
+      $('.tab-link').removeClass('c-active');
+      e.currentTarget.classList.add('c-active');
+      if (e.currentTarget.id === 'details') {
+        $('.detail-section').show();
+        $('.specification-section').hide();
+      } else {
+        $('.detail-section').hide();
+        $('.specification-section').show();
+      }
+    }
+  });
+
+  $('tab-link')
 });
-
-
-
