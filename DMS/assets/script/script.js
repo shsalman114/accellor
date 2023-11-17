836f58a6a64f57e69b0bb61b2e172624575d0ca5
shsalman114 <shaik.salman@accellor.com>
2023-11-17 11:52:02 +0530

    updated

--- DMS/assets/script/script.js
+++ DMS/assets/script/script.js
@@ -1,9 +1,35 @@
-$("#custom").on('click', () => {
-  console.log(this)
-  let cls = $(".tab-section")[0].lastElementChild.classList;
-  if (cls.contains('tab-hide')) {
-    cls.remove('tab-hide');
-  } else {
-    cls.add('tab-hide');
+// $(':radio').on('click', (event) => {
+//   let radioId = event.currentTarget.id;
+//   if (radioId === 'custom') {
+//     $('#spec').show();
+//     $(':button').on('click', () => {
+//       $('.tab-link').toggleClass('active');
+//     });
+//   } else {
+//     $('#spec').hide();
+//     if ($('#details').hasClass('active')) $('#details').toggleClass('active');
+//     else $('#details').toggleClass('active');
+//   }
+// });
+
+$(':radio').on('click', (event) => {
+  console.log('radio event')
+  console.log('radio event')
+  console.log('radio event')
+  if (event.currentTarget.id === 'custom') {
+    console.log('inside if')
+    $('#spec').show();
+    $(':button').on('click', () => {
+      $('.tab-link').toggleClass('active');
+    });
+    // if ($('#details').hasClass('active')) $('#details').toggleClass('active');
+    // else $('#details').toggleClass('active');
+  }
+  if (event.currentTarget.id === 'auto') {
+    console.log('radio else')
+    $('#spec').hide();
   }
 });
+
+
+
